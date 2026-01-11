d7948f64abf3c8c5286f536fead6b828f25d0409
Boishakhi G <bgmukta11@gmail.com>
2026-01-12 00:12:53 +0100

    routing added

---
+++ src/Router/Router.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from '../pages/Roots/Root';
+import Home from '../pages/Home/Home';
+import Apps from '../pages/Apps/Apps';
+import Installation from '../pages/Installation/Installation';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+        {
+            index: true, 
+            path: "/",
+            Component: Home
+        },
+        {
+            path: "Apps", Component: Apps
+        },
+        {
+            path: "Installation", Component: Installation
+        }
+    ]
+  },
+]);
